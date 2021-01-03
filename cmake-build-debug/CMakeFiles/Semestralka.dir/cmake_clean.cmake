3147ce9db2aa5a55f31929ddc6b4d560c5470cc4
Pavol <pavolhaviar4@gmail.com>
2021-01-03 16:51:59 +0100

    Pripojenie na server cez socket TEST1

---
+++ cmake-build-debug/CMakeFiles/Semestralka.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Semestralka.dir/main.c.o"
+  "Semestralka.pdb"
+  "Semestralka"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Semestralka.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,166 @@
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+
+
+int server(int argc, char *argv[]) {
+
+    printf("haloaaaa\n");
+    int sockfd, newsockfd;
+    socklen_t cli_len;
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+    char buffer[256];
+
+    if (argc < 2)
+    {
+        fprintf(stderr,"usage %s port\n", argv[0]);
+        return 1;
+    }
+
+    bzero((char*)&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(atoi(argv[1]));
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        perror("Error creating socket");
+        return 1;
+    }
+
+    if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        perror("Error binding socket address");
+        return 2;
+    }
+
+    listen(sockfd, 5);
+    cli_len = sizeof(cli_addr);
+
+    newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &cli_len);
+    if (newsockfd < 0)
+    {
+        perror("ERROR on accept");
+        return 3;
+    }
+
+    bzero(buffer,256);
+    n = read(newsockfd, buffer, 255);
+    if (n < 0)
+    {
+        perror("Error reading from socket");
+        return 4;
+    }
+    printf("Here is the message: %s\n", buffer);
+
+    const char* msg = "I got your message";
+    n = write(newsockfd, msg, strlen(msg)+1);
+    if (n < 0)
+    {
+        perror("Error writing to socket");
+        return 5;
+    }
+
+    close(newsockfd);
+    close(sockfd);
+
+}
+
+int client(int argc, char *argv[]) {
+
+    int sockfd, n;
+    struct sockaddr_in serv_addr;
+    struct hostent* server;
+
+    char buffer[256];
+
+    if (argc < 3)
+    {
+        fprintf(stderr,"usage %s hostname port\n", argv[0]);
+        return 1;
+    }
+
+    server = gethostbyname(argv[1]);
+    if (server == NULL)
+    {
+        fprintf(stderr, "Error, no such host\n");
+        return 2;
+    }
+
+    bzero((char*)&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy(
+            (char*)server->h_addr,
+            (char*)&serv_addr.sin_addr.s_addr,
+            server->h_length
+    );
+    serv_addr.sin_port = htons(atoi(argv[2]));
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        perror("Error creating socket");
+        return 3;
+    }
+
+    if(connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        perror("Error connecting to socket");
+        return 4;
+    }
+
+    printf("Please enter a message: ");
+    bzero(buffer,256);
+    fgets(buffer, 255, stdin);
+
+    n = write(sockfd, buffer, strlen(buffer));
+    if (n < 0)
+    {
+        perror("Error writing to socket");
+        return 5;
+    }
+
+    bzero(buffer,256);
+    n = read(sockfd, buffer, 255);
+    if (n < 0)
+    {
+        perror("Error reading from socket");
+        return 6;
+    }
+
+    printf("%s\n",buffer);
+    close(sockfd);
+
+
+}
+
+int main(int argc, char *argv[])
+{
+
+
+    /*pthread_t tcp_server;
+
+    pthread_create(&tcp_server,NULL ,server, 15000);
+    printf("Hello, World!\n");
+    printf("halo1\n");
+    pthread_t tcp_client;
+    pthread_create(&tcp_client,NULL,client,NULL);
+    printf("halo2\n");
+
+    //pthread_join(tcp_server,NULL);
+    //pthread_join(tcp_client,NULL);*/
+
+    printf("halo3\n");
+    server(2,15000);
+    return 0;
+}
+
+
