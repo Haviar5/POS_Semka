80fb979d3b5e64f60cb50285ce4829b0b85589b1
Delo <janosik27@stud.uniza.sk>
2021-01-05 11:38:14 +0100

    Klient funkčny server 17458

--- main.c
+++ main.c
@@ -1,84 +1,15 @@
-#include <pthread.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <netdb.h>
 
 
-int server(int argc, char *argv[]) {
 
 
-    int sockfd, newsockfd;
-    socklen_t cli_len;
-    struct sockaddr_in serv_addr, cli_addr;
-    int n;
-    char buffer[256];
-
-    if (argc < 2)
-    {
-        fprintf(stderr,"usage %s port\n", argv[0]);
-        return 1;
-    }
-
-
-    bzero((char*)&serv_addr, sizeof(serv_addr));
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    printf("0000\n");
-    serv_addr.sin_port = 15000;
-    printf("1111\n");
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-    {
-        perror("Error creating socket");
-        return 1;
-    }
-
-    if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        perror("Error binding socket address");
-        return 2;
-    }
-
-    listen(sockfd, 5);
-    cli_len = sizeof(cli_addr);
-    printf("2222\n");
-    newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &cli_len);
-    printf("3333\n");
-    if (newsockfd < 0)
-    {
-        perror("ERROR on accept");
-        return 3;
-    }
-    printf("4444\n");
-    bzero(buffer,256);
-    n = read(newsockfd, buffer, 255);
-    if (n < 0)
-    {
-        perror("Error reading from socket");
-        return 4;
-    }
-    printf("Here is the message: %s\n", buffer);
-
-    const char* msg = "I got your message";
-    n = write(newsockfd, msg, strlen(msg)+1);
-    if (n < 0)
-    {
-        perror("Error writing to socket");
-        return 5;
-    }
-
-    close(newsockfd);
-    close(sockfd);
-
-}
-
-int client(int argc, char *argv[]) {
+int main() {
 
     int sockfd, n;
     struct sockaddr_in serv_addr;
@@ -86,13 +17,9 @@ int client(int argc, char *argv[]) {
 
     char buffer[256];
 
-    if (argc < 3)
-    {
-        fprintf(stderr,"usage %s hostname port\n", argv[0]);
-        return 1;
-    }
 
-    server = gethostbyname(argv[1]);
+
+    server = gethostbyname("frios2.fri.uniza.sk");
     if (server == NULL)
     {
         fprintf(stderr, "Error, no such host\n");
@@ -106,7 +33,7 @@ int client(int argc, char *argv[]) {
             (char*)&serv_addr.sin_addr.s_addr,
             server->h_length
     );
-    serv_addr.sin_port = htons(atoi(argv[2]));
+    serv_addr.sin_port = 17458;
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
@@ -144,31 +71,7 @@ int client(int argc, char *argv[]) {
     close(sockfd);
 
 
-}
 
-int main(int argc, char *argv[])
-{
-
-
-    pthread_t tcp_server;
-
-    pthread_create(&tcp_server,NULL ,server(15000,1), NULL);
-    printf("Hello, World!\n");
-    printf("halo1\n");
-    pthread_t tcp_client;
-    pthread_create(&tcp_client,NULL,client,NULL);
-    printf("halo2\n");
-
-    //pthread_join(tcp_server,NULL);
-    //pthread_join(tcp_client,NULL);*/
-
-    //printf("halo3\n");
-    //server(15000,1);
-    //client(15000,1);
-
-
-    //TESTUJEME
-    return 0;
 }
 
 
